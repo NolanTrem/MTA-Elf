72cedd6ad90db756515773aec09086e1260f9470
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2020-07-27 20:05:30 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 /*MTA Data Search reads txt files of weekly MTA Data, searches for specific stations, and exports this data to Excel.
 * Pulls key data in the MTA designated format.
 * MTA turnstile data can be found at: http://web.mta.info/developers/turnstile.html
+* Nolan Tremelling, Columbia University 2020
 */
 import java.io.*;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -9,67 +10,31 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import javax.xml.crypto.Data;
 import java.util.*;
 
-class DataLine {
-    public String station;
-    public String getStation() {
-        return this.station;
-    }
-    public String linename;
-    public String getLinename() {
-        return this.linename;
-    }
-    public String date;
-    public String getDate(){
-        return this.date;
-    }
-    public String time;
-    public String getTime(){
-        return this.time;
-    }
-    public String entries;
-    public String getEntries(){
-        return this.entries;
-    }
-    public String exits;
-    public String getExits(){
-        return this.exits;
-    }
-
-    public DataLine(String Station, String Linename, String Date, String Time, String Entries, String Exits) {
-        this.station = Station;
-        this.linename = Linename;
-        this.date = Date;
-        this.time = Time;
-        this.entries = Entries;
-        this.exits = Exits;
-        //System.out.println(station + "\t" + linename + "\t" + date + "\t" + time + "\t" + entries + "\t" + exits);
-
-    }
-}
 
 public class Main {
     public static void main(String[] args)throws Exception {
         //Following lines can be customized based on user needs.
-        String[] columns = {"Station", "Linename", "Date", "Time", "Entries", "Exits"};
+        String[] columns = {"Station", "Linename", "Date", "Time", "Entries", "Exits", "Change in Time", "Change in Entries", "Change in Exits"};
         File file = new File("C:\\Users\\nolan\\Desktop\\COVID-19\\MTA Data Search\\turnstile_200620.txt");
         //Necessary declarations to read file
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
+        String station = "72 ST";    //Station name per MTA stylization
         List<DataLine> DataList = new ArrayList<>();
 
         while ((st = br.readLine()) != null){
-            //st.contains("") refers to target station name.
-            if (st.contains("72 ST")) {     // Change this string to search for other stations.
+            if (st.contains(station)) {     // Change this string to search for other stations.
                 String tokens[] = st.split(","); //MTA compiles data with separations by comma.
                 DataList.add(new DataLine(tokens[3], tokens[4], tokens[6], tokens[7], tokens[9], tokens[10])); //Target data
+
                 //Sets up Excel workbook.
                 Workbook workbook = new XSSFWorkbook();
                 CreationHelper createHelper = workbook.getCreationHelper();
-                Sheet sheet = workbook.createSheet("TITLE");
+                Sheet sheet = workbook.createSheet(station);
                 Font headerFont = workbook.createFont();
                 headerFont.setBold(true);
                 headerFont.setFontHeightInPoints((short) 14);
-                headerFont.setColor(IndexedColors.RED.getIndex());
+                headerFont.setColor(IndexedColors.ROYAL_BLUE.getIndex());
                 CellStyle headerCellStyle = workbook.createCellStyle();
                 headerCellStyle.setFont(headerFont);
                 Row headerRow = sheet.createRow(0);
@@ -80,6 +45,8 @@ public class Main {
                     cell.setCellStyle(headerCellStyle);
                 }
 
+
+
                 int rowNum = 1;
                 for (DataLine dataLine: DataList){
                     Row row = sheet.createRow(rowNum++);
@@ -89,14 +56,26 @@ public class Main {
                     row.createCell(3).setCellValue(dataLine.getTime());
                     row.createCell(4).setCellValue(dataLine.getEntries());
                     row.createCell(5).setCellValue(dataLine.getExits());
+                    row.createCell(6).setCellValue("hello");
+                    int entriesLow = 2;
+                    int entriesHigh = 3;
+                    int exitsLow = 2;
+                    int exitsHigh = 3;
+                    int asdf = 0;
+                    while (asdf < rowNum){
+                        entriesLow++;
+                        entriesHigh++;
+                        exitsLow++;
+                        exitsHigh++;
+                        sheet.autoSizeColumn(asdf);
+                        row.createCell(7).setCellFormula("E" + entriesHigh + " - E" + entriesLow);
+                    }
+                    row.createCell(8).setCellValue("hello");
                 }
 
-                for(int i = 0; i < columns.length; i++){
-                    sheet.autoSizeColumn(i);
-                }
 
                 //Change the following line to change the title of the Excel file.
-                FileOutputStream fileOut = new FileOutputStream("poi-generated.xlsx");
+                FileOutputStream fileOut = new FileOutputStream(station + ".xlsx");
                 workbook.write(fileOut);
                 fileOut.close();
                 workbook.close();
